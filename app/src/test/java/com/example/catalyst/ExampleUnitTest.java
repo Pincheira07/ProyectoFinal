93fa53028850807a6257e8a0e3c1572e308d186c
Pincheira07 <m.pincheira07@ufromail.cl>
2021-05-25 02:25:19 -0400

    Commit 2

--- app/src/test/java/com/example/catalyst/ExampleUnitTest.java
+++ app/src/test/java/com/example/catalyst/ExampleUnitTest.java
@@ -1,14 +1,9 @@
 package com.example.catalyst;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
